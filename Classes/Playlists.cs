bbb91d8539a826ca2920e95e3c52afd8fa6a5f21
HYPERJAK <james.jakthesamething@gmail.com>
2023-06-10 19:02:21 +0300

    Added testing panel and playlist planning

---
+++ Classes/Playlists.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NiceUIDesign.Classes
+{
+    public class Playlists : FlowLayoutPanel
+    {
+
+        public Playlists()
+        {
+            this.Anchor = AnchorStyles.Top | AnchorStyles.Left;
+            this.Dock = DockStyle.Fill;
+            this.BackColor = Color.AliceBlue;
+            this.Size = new Size(451, 558);
+            this.TabIndex = 1;
+            this.FlowDirection = FlowDirection.LeftToRight;
+            this.WrapContents = true;
+            this.AllowDrop = true;
+
+            add_song("hi");
+            add_song("hi2");
+            add_song("hi3");
+
+        }
+
+        public void init_options()
+        {
+
+        }
+
+
+        public void add_song(string songName)
+        {
+            Button test = new Button { Name = $"button{songName}", Text = $"This is button {songName}" };
+            ButtonsTracker buttonTrack = new ButtonsTracker();
+            buttonTrack.addButton(test);
+            this.Controls.Add(test);
+
+        }
+    }
+
+
+}
