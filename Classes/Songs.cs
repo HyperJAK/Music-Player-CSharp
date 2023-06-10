854ec0bf6ebd6a7e221ada89de68fdaef0ea10da
HYPERJAK <james.jakthesamething@gmail.com>
2023-06-10 19:01:29 +0300

    Modified dictionary structure

--- Classes/Songs.cs
+++ Classes/Songs.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +10,11 @@ using System.Windows.Forms;
 
 namespace NiceUIDesign.Classes
 {
-    internal class Songs : FlowLayoutPanel
+    public class Songs : FlowLayoutPanel
     {
+
+        private Dictionary<string, int> songs_dict = new Dictionary<string, int>();
+            
         public Songs() {
             this.Anchor = AnchorStyles.Top | AnchorStyles.Left;
             this.Dock = DockStyle.Fill;
@@ -39,6 +44,39 @@ namespace NiceUIDesign.Classes
             buttonTrack.addButton(test);
             this.Controls.Add(test);
 
+
+
+            var jsonData = new
+            {
+                song_ids = songs_dict
+            };
+
+
+            songs_dict.Add($"{songName}", songs_dict.Count() + 1);
+
+            string json = JsonConvert.SerializeObject(jsonData, Formatting.Indented);
+            File.WriteAllText("dictionary.json", json);
+
+            GetSongs();
+
+        }
+
+        public void GetSongs()
+        {
+            string json2 = File.ReadAllText("dictionary.json");
+
+            var jsonData2 = JsonConvert.DeserializeObject<dynamic>(json2);
+
+            // Access the group name "song_ids" and iterate over the songs_dict dictionary
+            var songIds = jsonData2.song_ids;
+
+            foreach (var song in songIds)
+            {
+                string songName2 = song.Name;
+                int songId = song.Value;
+
+                Console.WriteLine($"Song Name: {songName2}, Song ID: {songId}");
+            }
         }
 
 
